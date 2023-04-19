c57cfebd8487d18d419af383406d9608e032fd42
Triweekly <111813883+Triweekly@users.noreply.github.com>
2023-04-18 21:52:02 -0400

    copied

---
+++ RTSProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author KRLor
+ *
+ */
+module RTSProject {
+}
---
+++ RTSProject/src/simulator/Job.java
@@ -0,0 +1,16 @@
+package simulator;
+
+public class Job{
+	int c;	//completion time == duration
+	int a;	//arrival time
+	int d;	//deadline
+	int s;	//start time
+	int f;	//finish time
+	
+	public Job(int computationTime, int arrivalTime, int deadline) 
+	{
+		c = computationTime;
+		a = arrivalTime;
+		d = deadline;
+	}
+} 
